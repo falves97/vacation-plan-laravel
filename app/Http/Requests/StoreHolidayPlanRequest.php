3dc5b9c218aecba5ee8bc499490c1aec203ae0d1
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-05-01 21:55:36 -0300

    feat: request para validação de store de HolidayPlan

---
+++ app/Http/Requests/StoreHolidayPlanRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreHolidayPlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'string|required|max:255',
+            'description' => 'string|required|max:255',
+            'location' => 'string|required|max:255',
+            'date' => 'dateformat:d/m/Y|required',
+            'participants' => 'array|nullable',
+            'participants.*' => 'email|required|max:255|exists:users,email',
+        ];
+    }
+}
