cdb33419b9c2feca3db779ebc130d7d8df70d2dc
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-05-01 21:55:06 -0300

    feat: DTO de HolidayPlan

---
+++ app/DTOs/HolidayPlanDTO.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+
+final class HolidayPlanDTO
+{
+    /**
+     * @param string $title
+     * @param string $description
+     * @param string $location
+     * @param Carbon $date
+     * @param User $owner
+     * @param Collection<User> $participants
+     */
+    public function __construct(
+        public readonly string     $title,
+        public readonly string     $description,
+        public readonly string     $location,
+        public readonly Carbon     $date,
+        public readonly User       $owner,
+        public readonly Collection $participants,
+    ) {
+    }
+}
