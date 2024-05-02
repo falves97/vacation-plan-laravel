a12879da52542e092db5f3f63487aed7646f300f
Fernando Braga Alves <fbragaalves97@gmail.com>
2024-05-01 21:54:16 -0300

    feat: adicionado método para gerar uma collection de participantes a partir de um array de emails

--- app/Services/HolidayPlanService.php
+++ app/Services/HolidayPlanService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\DTOs\HolidayPlanDTO;
 use App\Models\HolidayPlan;
 use App\Repositories\UserRepository;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -9,7 +10,30 @@ use Illuminate\Support\Collection;
 
 class HolidayPlanService
 {
-    public function syncHolidayPlanWithParticipants(HolidayPlan $holidayPlan, Collection $participants): HolidayPlan
+    public function __construct(private readonly UserRepository $userRepository)
+    {
+    }
+
+    public static function createHolidayPlan(HolidayPlanDTO $holidayPlanDto): HolidayPlan
+    {
+        /** @var HolidayPlan $holidayPlan */
+        $holidayPlan =  HolidayPlan::query()->create([
+            'title' => $holidayPlanDto->title,
+            'description' => $holidayPlanDto->description,
+            'date' => $holidayPlanDto->date,
+            'location' => $holidayPlanDto->location,
+            'owner_id' => $holidayPlanDto->owner->id,
+        ]);
+
+        return $holidayPlan;
+    }
+
+    /**
+     * @param HolidayPlan $holidayPlan
+     * @param Collection $participants
+     * @return HolidayPlan
+     */
+    public static function syncHolidayPlanWithParticipants(HolidayPlan $holidayPlan, Collection $participants): HolidayPlan
     {
         $participantsId = $participants
             // Add owner as participant
